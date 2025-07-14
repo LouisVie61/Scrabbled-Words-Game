933f76b7d28c41af7a8d658068c6c2bfee91a123
LouisVie61 <tantrieucao@gmail.com>
2025-07-14 14:43:08 +0700

    feat: build and run the first program

--- main.cpp
+++ main.cpp
@@ -2,7 +2,6 @@
 #include <SDL3/SDL_events.h>
 #include <SDL3/SDL_render.h>
 #include <SDL3/SDL_video.h>
-#include <SDL3/SDL_error.h>
 #include <SDL3/SDL_init.h>
 #include <SDL3/SDL_keycode.h>
 #include <SDL3_image/SDL_image.h>
